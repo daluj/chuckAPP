773eec1d85d09223a535d2ab7cdaeec349e161d7
david <davidluengo8@gmail.com>
2020-10-23 14:18:16 +0200

    test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -23,4 +23,57 @@ class ExampleTest extends TestCase
         $response->assertSee('Chuck Norris Chistes');
         $response->assertViewIs('home');
     }
+
+    public function testRandomJokeNoEmail() {
+        $query = array(
+            'action' => 'random'
+        );
+        $response = $this->get('/en/chuck?'.http_build_query($query));
+        $response->assertSee('Facts');
+        $response->assertViewIs('chuck');
+
+        $response = $this->get('/es/chuck?'.http_build_query($query));
+        $response->assertSee('Chistes');
+        $response->assertViewIs('chuck');
+    }
+
+    public function testCategoryJokeNoEmail() {
+        $query = array(
+            'action' => 'category',
+            'category' => 'fashion'
+        );
+        $response = $this->get('/en/chuck?'.http_build_query($query));
+        $response->assertSee('Facts');
+        $response->assertViewIs('chuck');
+
+        $response = $this->get('/es/chuck?'.http_build_query($query));
+        $response->assertSee('Chistes');
+        $response->assertViewIs('chuck');
+    }
+
+    public function testQueryJokeNoEmail() {
+        $query = array(
+            'action' => 'words',
+            'word'   => 'animal'
+        );
+        $response = $this->get('/en/chuck?'.http_build_query($query));
+        $response->assertSee('Facts');
+        $response->assertViewIs('chuck');
+
+        $response = $this->get('/es/chuck?'.http_build_query($query));
+        $response->assertSee('Chistes');
+        $response->assertViewIs('chuck');
+    }
+
+    public function testRandomJokeSendEmail() {
+        $query = array(
+            'action' => 'random',
+            'email'  => 'test@email.com'
+        );
+        $response = $this->get('/en/chuck?'.http_build_query($query));
+        $response->assertSee('Email sent');
+
+        $response = $this->get('/es/chuck?'.http_build_query($query));
+        $response->assertSee('Email enviado');
+    }
 }
